5adaba4fe2ef4fac265041285893d7c322b4ab04
Sagar Khadka <64532132+sagarjk07@users.noreply.github.com>
2026-05-11 12:36:10 +1000

    Update server.js

--- server.js
+++ server.js
@@ -7,11 +7,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: ["https://beforeyousay.com", "https://www.beforeyousay.com"],
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
-}));
+app.use(cors());
 
 app.use(express.json({ limit: "1mb" }));
 
